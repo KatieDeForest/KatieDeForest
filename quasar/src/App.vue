c3433ed74656863d9c9922089e2389b6cb69eee2
KatieDeForest <KatieDeForest@outlook.com>
2025-10-05 15:24:15 +0000

    We use router-view instead

--- quasar/src/App.vue
+++ quasar/src/App.vue
@@ -1,30 +1,8 @@
-<script setup lang="ts">
-import 'mdb-vue-ui-kit/css/mdb.min.css';
-// App component logic: imports and sets up main layout
-import GalleryShortcut from './components/GalleryShortcut.vue';
-import SiteHeader from './components/SiteHeader.vue';
-import HeroBanner from './components/HeroBanner.vue';
-import SiteFooter from './components/SiteFooter.vue';
-</script>
 
 <template>
-  <SiteHeader />
-  <HeroBanner />
-  <GalleryShortcut />
-  <SiteFooter />
+  <router-view />
 </template>
 
-<style scoped>
-/* App component styles: font and layout */
-#app {
-  font-family: Roboto, Helvetica, Arial, sans-serif;
-}
-</style>
-
-<style>
-body, html {
-  margin: 0;
-  height: 100%;
-  overflow-x: hidden;
-}
-</style>
+<script setup lang="ts">
+//
+</script>
