13a76d9e61f6aae2c107628e1e7299e7fc4e04d6
KatieDeForest <KatieDeForest@outlook.com>
2025-10-08 19:52:47 +0000

    Added a Gallery category page, along with a gallery grid system for it.

---
+++ quasar/src/pages/GalleryCatPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <GalleryHeroBanner />
+    <GalleryCard />
+    <GallerySearchReminder />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import GalleryCard from 'src/components/GalleryCard.vue';
+import GalleryHeroBanner from 'src/components/GalleryHeroBanner.vue';
+import GallerySearchReminder from 'src/components/GallerySearchReminder.vue';
+</script>
