1e81a66688d78d0a9b5e5fd8fbdbe6f0bfaa79a4
KatieDeForest <KatieDeForest@outlook.com>
2025-10-15 22:47:56 +0000

    Added scroll to top feature

--- quasar/src/pages/IndexPage.vue
+++ quasar/src/pages/IndexPage.vue
@@ -2,10 +2,12 @@
   <q-page>
     <MainHeroBanner />
     <GalleryShortcut />
+    <ScrollToTop />
   </q-page>
 </template>
 
 <script setup lang="ts">
 import MainHeroBanner from 'components/MainHeroBanner.vue';
 import GalleryShortcut from 'components/GalleryShortcut.vue';
+import ScrollToTop from 'components/ScrollToTop.vue';
 </script>
