6c5d3a95f37ca82244ec984a9f6f740577782545
KatieDeForest <KatieDeForest@outlook.com>
2025-10-22 23:14:42 +0000

    Fixing an issue where q-page would overwrite the css if min-height wasn't over 700px in the page, for example for the 404 page that doesnt need to be 700px in height

--- quasar/src/pages/ErrorNotFoundPage.vue
+++ quasar/src/pages/ErrorNotFoundPage.vue
@@ -1,9 +1,24 @@
 <template>
   <q-page>
     <ErrorNotFound/>
+    <SearchReminder/>
   </q-page>
 </template>
 
+<style scoped lang="scss">
+.q-page {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  color: $primary;
+  background-color: $primary;
+}
+</style>
+
+
 <script setup lang="ts">
 import ErrorNotFound from 'src/components/ErrorNotFound.vue';
+import SearchReminder from 'src/components/SearchReminder.vue';
 </script>
